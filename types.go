d1731479ad825beea1858dc4a0b96851273c4771
Jay Rexiti <jrexiti@totalwine.com>
2023-03-14 12:32:46 -0400

    playing with types

---
+++ types.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var myInt int
+var myInt32 int32
+var myInt64 int64
+
+func types() {
+	fmt.Printf("type of myInt: %T\n", myInt)
+	fmt.Printf("type of myInt32: %T\n", myInt32)
+	fmt.Printf("type of myInt64: %T\n", myInt64)
+
+}
