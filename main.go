606ca07071133a2ea84210585b8ef188b2c0cede
Jay Rexiti <jrexiti@totalwine.com>
2023-03-14 11:26:35 -0400

    added boiler plate game

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
-	menu()
+	"www.github.com/jrexiti/go-basics/game"
+)
+
+func main() {
+	playAgain := true
+
+	for playAgain {
+		game.Play()
+		playAgain = game.GetYesOrNo("Would you like to play again (y/n)?")
+	}
+
+	fmt.Println("")
+	fmt.Println("Goodbye.")
 }
