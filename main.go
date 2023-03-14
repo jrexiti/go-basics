863b153d0e7f44b105d5f750b7d2a3d32f18fa17
jrexiti <jay.rexiti@gmail.com>
2023-03-14 01:47:25 -0400

    commit

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/eiannone/keyboard"
 )
@@ -16,20 +17,28 @@ func main() {
 	defer func() {
 		_ = keyboard.Close()
 	}()
-	fmt.Println("Press any key on the keyboard. Press ESC to quit")
+	fmt.Println("Menu")
+	fmt.Println("____")
+
+	fmt.Println("1 - Coffee")
+	fmt.Println("2 - Latte")
+	fmt.Println("3 - Cappucino")
+	fmt.Println("4 - Chai tea")
+	fmt.Println("5 - Tea")
+	fmt.Println("5 - Americano")
+	fmt.Println("Q - Quit the program")
 
 	for {
-		char, key, err := keyboard.GetSingleKey()
+		char, _, err := keyboard.GetSingleKey()
 		if err != nil {
 			log.Fatal(err)
 		}
-		if key != 0 {
-			fmt.Println("You pressed:", char, key)
-		} else {
-			fmt.Println("You pressed:", char)
 
-		}
-		if key == keyboard.KeyEsc {
+		i, _ := strconv.Atoi(string(char))
+		t := fmt.Sprintf("You chose %d", i)
+		fmt.Println(t)
+
+		if char == 'q' || char == 'Q' {
 			break
 		}
 	}
