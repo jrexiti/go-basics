659089895e00c611cc92ea2044f5dff0a18e8c4c
jrexiti <jay.rexiti@gmail.com>
2023-03-14 01:09:31 -0400

    new project

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
-
-	"www.github.com/jrexiti/go-basics/packageone"
+	"bufio"
+	"os"
 )
 
 func main() {
-	packageone.Exported()
-	rand.Seed(time.Now().UnixNano())
-	newstring := packageone.PublicVar
-	fmt.Println("From packageone:", newstring)
-	aString := packageone.PublicVar
-	fmt.Println(aString)
-	var firstNumber = rand.Intn(8) + 2
-	var secondNumber = rand.Intn(8) + 2
-	var subtraction = rand.Intn(8) + 2
-	var answer = firstNumber*secondNumber - subtraction
-	theGame(firstNumber, secondNumber, subtraction, answer)
-
+	render := bufio.NewReader(os.Stdin)
 }
