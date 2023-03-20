ce33de43b922244cdec5fb4fc4223e92151fd1f3
Jay Rexiti <jrexiti@totalwine.com>
2023-03-20 17:10:05 -0400

    pointers and concurrency

--- main.go
+++ main.go
@@ -4,4 +4,21 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world!")
+
+	var a = 1
+	var b = &a
+	*b = 4
+	a = 9
+
+	fmt.Println(b, *b, a)
+
+	myChannel := make(chan string)
+
+	go func() {
+		myChannel <- "data"
+	}()
+
+	msg := <-myChannel
+
+	fmt.Println(msg)
 }
